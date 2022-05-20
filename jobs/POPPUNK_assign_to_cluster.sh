d1c06730efe7d941ed6c6d784a53b452671eef91
Sam Farrar <samefarrar@gmail.com>
2022-05-20 15:46:08 +0100

    Updates DB but no visualize

--- jobs/POPPUNK_assign_to_cluster.sh
+++ jobs/POPPUNK_assign_to_cluster.sh
@@ -3,7 +3,7 @@
 #PBS -o poppunk_assign_to_cluster.o
 #PBS -e poppunk_assign_to_cluster.e
 #PBS -l walltime=72:00:00
-#PBS -l select=1:ncpus=16:mem=48gb
+#PBS -l select=1:ncpus=16:mem=360gb
 
 module load anaconda3/personal
 
@@ -11,7 +11,4 @@ source activate poppunk
 
 cd /rds/general/user/sef17/home/msc_diss/data/gen/assemblies
 
-poppunk_assign --db /rds/general/user/sef17/home/msc_diss/ref/reference_db/GPS_v5 --query /rds/general/user/sef17/home/msc_diss/data/gen/assemblies/poppunk_12f.txt --output /rds/general/user/sef17/home/msc_diss/result/query_assignment --threads 8
-
-poppunk_visualise --ref-db /rds/general/user/sef17/home/msc_diss/ref/reference_db/GPS_v5 --query-db /rds/general/user/sef17/home/msc_diss/result/query_assignment --output /rds/general/user/sef17/home/msc_diss/result/query_assignment --microreact --threads 8
-
+poppunk_assign --db /rds/general/user/sef17/home/msc_diss/ref/reference_db/GPS_v5 --query /rds/general/user/sef17/home/msc_diss/data/gen/assemblies/poppunk_12f.txt --output /rds/general/user/sef17/home/msc_diss/result/poppunk_clusters --threads 8 --update-db
