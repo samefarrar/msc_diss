2e17511193ba3c8da3e42990420c353d30634918
Sam Farrar <samefarrar@gmail.com>
2022-05-21 13:11:26 +0100

    Visualization submission script

---
+++ jobs/POPPUNK_visualize.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+#PBS -N poppunk_visualize
+#PBS -o poppunk_visualize.o
+#PBS -e poppunk_visualize.e
+#PBS -l walltime=72:00:00
+#PBS -l select=1:ncpus=16:mem=360gb
+
+module load anaconda3/personal
+
+source activate poppunk
+
+cd /rds/general/user/sef17/home/msc_diss/data/gen/assemblies
+
+poppunk_visualise --ref-db /rds/general/user/sef17/home/msc_diss/result/poppunk_clusters --output /rds/general/user/sef17/home/msc_diss/result/poppunk_visualize  --threads 8 --microreact
