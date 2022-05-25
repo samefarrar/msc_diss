7788237b365902016c1587257de41ec8d4f6b6db
Sam Farrar <samefarrar@gmail.com>
2022-05-25 19:56:20 +0100

    New query assign job, does not update db (outputs to batch2

---
+++ jobs/POPPUNK_query_assign.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+#PBS -N poppunk_assign_to_cluster
+#PBS -o poppunk_assign_to_cluster.o
+#PBS -e poppunk_assign_to_cluster.e
+#PBS -l walltime=72:00:00
+#PBS -l select=1:ncpus=16:mem=360gb
+
+module load anaconda3/personal
+
+source activate poppunk
+
+cd /rds/general/user/sef17/home/msc_diss/data/gen/assemblies
+
+poppunk_assign --db /rds/general/user/sef17/home/msc_diss/ref/reference_db/GPS_v5 --query /rds/general/user/sef17/home/msc_diss/data/gen/assemblies/poppunk_12f.txt --output /rds/general/user/sef17/home/msc_diss/result/batch2 --threads 8 --external-clustering /rds/general/user/sef17/home/msc_diss/ref/reference_db/GPS_v5_external_clusters.csv 
