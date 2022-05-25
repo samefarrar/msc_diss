ae2f1388fad082af7f75a047b2d598341ae63b99
Sam Farrar <samefarrar@gmail.com>
2022-05-25 11:26:23 +0100

    Fixed array job to take local directory

--- jobs/spades_array.sh
+++ jobs/spades_array.sh
@@ -1,9 +1,10 @@
-#PBS -o SPAdes/logs/hsa_spades.^array_index^.run.o
-#PBS -e SPAdes/logs/hsa_spades.^array_index^.run.e
+
+#PBS -o SPAdes/hsa_spades.^array_index^.run.o
+#PBS -e SPAdes/hsa_spades.^array_index^.run.e
 #PBS -l walltime=72:00:00
 #PBS -l select=1:ncpus=2:mem=16gb
 #PBS -N hsa_spades_job
 #PBS -J 1-1336
 
-/rds/general/user/ncrouche/home/WORK/pathogen_surveillance/live/12F_epidemiology/fastqs/spades_job.${PBS_ARRAY_INDEX}
+/rds/general/user/sef17/home/msc_diss/jobs/SPAdes/hsa_spades_job.${PBS_ARRAY_INDEX}
 
