bcde65d07b4d98f8de97a0eff9cdaea1626b6ee6
Sam Farrar <samefarrar@gmail.com>
2022-05-25 08:59:38 +0100

    Generate trim-generation script

---
+++ jobs/eg_trim_gen_script.sh
@@ -0,0 +1,35 @@
+# declare an integer object
+declare -i N;
+N=1;
+# iterate all raw fastq files
+for f in *_R1.fastq.gz; do
+  #define variables
+  ISOLATE=${f%_R1.fastq.gz};
+  FORWARD_FASTQ=${f};
+  REVERSE_FASTQ=${f%_R1.fastq.gz}_R2.fastq.gz;
+  # print trimmomatic jobs for all fastqs and run
+  echo -en "module load anaconda3/personal\nsource activate trim039\ncd /rds/general/user/sef17/home/msc_diss/data/orig/12F_epidemiology/raw_fastqs\njava -jar /rds/general/user/sef17/home/msc_diss/ref/Trimmomatic-0.39/dist/jar/trimmomatic-0.39.jar PE -threads 8 -phred33 ${FORWARD_FASTQ}  ${REVERSE_FASTQ} /rds/general/user/sef17/home/msc_diss/data/gen/trimmed/${ISOLATE}_R1.fastq.gz /rds/general/user/sef17/home/msc_diss/data/gen/trimmed/${ISOLATE}_R1_unpaired.fastq.gz /rds/general/user/sef17/home/msc_diss/data/gen/trimmed/${ISOLATE}_R2.fastq.gz /rds/general/user/sef17/home/msc_diss/data/gen/trimmed/${ISOLATE}_R2_unpaired.fastq.gz" > /rds/general/user/sef17/home/msc_diss/jobs/Trim/trim_job.$N;
+  chmod 770 /rds/general/user/sef17/home/msc_diss/jobs/Trim/trim_job.$N;
+  N=($N+1);
+done
+# get the correct number of jobs to run
+N=($N-1)
+
+echo "
+#PBS -o trim.^array_index^.run.o
+#PBS -e trim.^array_index^.run.e
+#PBS -l walltime=72:00:00
+#PBS -l select=1:ncpus=4:mem=16gb
+#PBS -N trim_job
+#PBS -J 1-$N
+
+/rds/general/user/sef17/home/msc_diss/jobs/Trim/trim_job.\${PBS_ARRAY_INDEX}
+" > /rds/general/user/sef17/home/msc_diss/jobs/trim_array.sh
+
+for f in *.streptococcus-pneumoniae-typing.ngsservice.processed.R1.fastq.gz; do
+  #define variables
+  ISOLATE=${f%.streptococcus-pneumoniae-typing.ngsservice.processed.R1.fastq.gz};
+  # print trimmomatic jobs for all fastqs and run
+  mv ${f} ${ISOLATE}_R1.fastq.gz
+  mv ${ISOLATE}.streptococcus-pneumoniae-typing.ngsservice.processed.R2.fastq.gz ${ISOLATE}_R2.fastq.gz
+done
