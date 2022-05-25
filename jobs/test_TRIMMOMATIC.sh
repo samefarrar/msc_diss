297a473b508a14e4acd85319386e2a61b1f3bf0b
Sam Farrar <samefarrar@gmail.com>
2022-05-25 10:15:11 +0100

    Specified exact trimmomatic path

--- jobs/test_TRIMMOMATIC.sh
+++ jobs/test_TRIMMOMATIC.sh
@@ -6,4 +6,5 @@
 module load anaconda3/personal
 source activate trim039
 
-java -jar ../ref/Trimmomatic-0.39/dist/jar/trimmomatic-0.39.jar PE -threads 4 -phred33 ../data/orig/12F_epidemiology/fastqs/975747_1.fastq.gz
+cd /rds/general/user/sef17/home/msc_diss/data/orig/12F_epidemiology/raw_fastqs
+java -jar /rds/general/user/sef17/home/msc_diss/ref/Trimmomatic-0.39/dist/jar/trimmomatic-0.39.jar PE -threads 8 -phred33 /rds/general/user/sef17/home/msc_diss/data/orig/12F_epidemiology/raw_fastqs/220448_H16046042510-1_R1.fastq.gz  /rds/general/user/sef17/home/msc_diss/data/orig/12F_epidemiology/raw_fastqs/220448_H16046042510-1_R2.fastq.gz /rds/general/user/sef17/home/msc_diss/data/gen/trimmed/220448_H16046042510-1_R1.fastq.gz /rds/general/user/sef17/home/msc_diss/data/gen/trimmed/220448_H16046042510-1_R1_unpaired.fastq.gz /rds/general/user/sef17/home/msc_diss/data/gen/trimmed/220448_H16046042510-1_R2.fastq.gz /rds/general/user/sef17/home/msc_diss/data/gen/trimmed/220448_H16046042510-1_R2_unpaired.fastq.gz ILLUMINACLIP:/rds/general/user/sef17/home/msc_diss/ref/Trimmomatic-0.39/adapters/TruSeq3-PE.fa:2:30:10:2:True LEADING:3 TRAILING:3 MINLEN:36
