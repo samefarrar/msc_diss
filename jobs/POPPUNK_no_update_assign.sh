123444752a8b59ceece1820cb654c2da3e9077bf
Sam Farrar <samefarrar@gmail.com>
2022-06-01 16:49:20 +0100

    Uodate to include all isolates

--- jobs/POPPUNK_no_update_assign.sh
+++ jobs/POPPUNK_no_update_assign.sh
@@ -3,7 +3,7 @@
 #PBS -o poppunk_no_update.o
 #PBS -e poppunk_no_update.e
 #PBS -l walltime=72:00:00
-#PBS -l select=1:ncpus=16:mem=400gb
+#PBS -l select=1:ncpus=16:mem=750gb
 
 module load anaconda3/personal
 
@@ -11,4 +11,4 @@ source activate poppunk
 
 cd /rds/general/user/sef17/home/msc_diss/data/gen/assemblies
 
-poppunk_assign --db /rds/general/user/sef17/home/msc_diss/ref/reference_db/GPS_v5 --query /rds/general/user/sef17/home/msc_diss/data/gen/assemblies/poppunk_12f.txt --output /rds/general/user/sef17/home/msc_diss/result/no_update --threads 8 --external-clustering /rds/general/user/sef17/home/msc_diss/ref/reference_db/GPS_v5_external_clusters.csv 
+poppunk_assign --db /rds/general/user/sef17/home/msc_diss/ref/reference_db/GPS_v5 --query /rds/general/user/sef17/home/msc_diss/data/gen/assemblies/all_12f.txt --output /rds/general/user/sef17/home/msc_diss/result/poppunk_nu --threads 12 --external-clustering /rds/general/user/sef17/home/msc_diss/ref/reference_db/GPS_v5_external_clusters.csv 
