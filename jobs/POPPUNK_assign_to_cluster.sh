93578697b09d5f3883b4f984cdee01ed65739b71
Sam Farrar <samefarrar@gmail.com>
2022-06-01 16:47:17 +0100

    New assign to cluster with all isolates

--- jobs/POPPUNK_assign_to_cluster.sh
+++ jobs/POPPUNK_assign_to_cluster.sh
@@ -3,7 +3,7 @@
 #PBS -o poppunk_assign_to_cluster.o
 #PBS -e poppunk_assign_to_cluster.e
 #PBS -l walltime=72:00:00
-#PBS -l select=1:ncpus=16:mem=360gb
+#PBS -l select=1:ncpus=16:mem=1000gb
 
 module load anaconda3/personal
 
@@ -11,4 +11,4 @@ source activate poppunk
 
 cd /rds/general/user/sef17/home/msc_diss/data/gen/assemblies
 
-poppunk_assign --db /rds/general/user/sef17/home/msc_diss/ref/reference_db/GPS_v5 --query /rds/general/user/sef17/home/msc_diss/data/gen/assemblies/poppunk_12f.txt --output /rds/general/user/sef17/home/msc_diss/result/batch1 --threads 8 --update-db
+poppunk_assign --db /rds/general/user/sef17/home/msc_diss/ref/reference_db/GPS_v5 --query /rds/general/user/sef17/home/msc_diss/data/gen/assemblies/all_12f.txt --output /rds/general/user/sef17/home/msc_diss/result/poppunk_clusters --threads 8 --update-db
