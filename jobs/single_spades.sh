4fc5c7c4972e04f96518448ff6fdfa640787b31d
Sam Farrar <samefarrar@gmail.com>
2022-06-06 15:12:07 +0100

    Single SPADES run for the one job that didn't work

---
+++ jobs/single_spades.sh
@@ -0,0 +1,7 @@
+#PBS -o SPAdes/hsa_spades.single.run.o
+#PBS -e SPAdes/hsa_spades.single.run.e
+#PBS -l walltime=12:00:00
+#PBS -l select=1:ncpus=2:mem=16gb
+#PBS -N hsa_spades_job
+
+/rds/general/user/sef17/home/msc_diss/jobs/SPAdes/hsa_spades_job.37
