0abac22f339b4f2f423d6b436bdd875b1e811b8d
Sam Farrar <samefarrar@gmail.com>
2022-06-09 15:45:40 +0100

    8 -> 16 threads

--- jobs/POPPUNK_assign_to_cluster.sh
+++ jobs/POPPUNK_assign_to_cluster.sh
@@ -11,4 +11,4 @@ source activate poppunk
 
 cd /rds/general/user/sef17/home/msc_diss/data/gen/assemblies
 
-poppunk_assign --db /rds/general/user/sef17/home/msc_diss/ref/reference_db/GPS_v5 --query /rds/general/user/sef17/home/msc_diss/data/gen/assemblies/all_12f.txt --output /rds/general/user/sef17/home/msc_diss/result/poppunk_clusters --threads 8 --update-db
+poppunk_assign --db /rds/general/user/sef17/home/msc_diss/ref/reference_db/GPS_v5 --query /rds/general/user/sef17/home/msc_diss/data/gen/assemblies/all_12f.txt --output /rds/general/user/sef17/home/msc_diss/result/poppunk_clusters --threads 16 --update-db
