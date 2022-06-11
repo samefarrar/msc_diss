2a2fd5cb4987741d757cb98d48de76a15b96086f
Sam Farrar <samefarrar@gmail.com>
2022-06-11 10:20:38 +0100

    New SPADES Array

--- jobs/spades_array.sh
+++ jobs/spades_array.sh
@@ -1,10 +1,10 @@
-
-#PBS -o SPAdes/hsa_spades.^array_index^.run.o
-#PBS -e SPAdes/hsa_spades.^array_index^.run.e
+#! /bin/sh
+#PBS -o SPAdes/logs/spades.^array_index^.run.o
+#PBS -e SPAdes/logs/spades.^array_index^.run.e
 #PBS -l walltime=72:00:00
 #PBS -l select=1:ncpus=2:mem=16gb
-#PBS -N hsa_spades_job
-#PBS -J 1-1336
+#PBS -N spades_array_job
+#PBS -J 1-1400
 
-/rds/general/user/sef17/home/msc_diss/jobs/SPAdes/hsa_spades_job.${PBS_ARRAY_INDEX}
+/rds/general/user/sef17/home/msc_diss/jobs/SPAdes/spades_job.${PBS_ARRAY_INDEX}
 
