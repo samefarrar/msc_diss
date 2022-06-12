2307a6cb443b63358f77910d9a9003b7cd6684a2
Sam Farrar <samefarrar@gmail.com>
2022-06-12 17:38:08 +0100

    Generate txt file to end of generation command

--- jobs/gen_assembly_stats_run.sh
+++ jobs/gen_assembly_stats_run.sh
@@ -4,3 +4,4 @@ for d in *_spades/; do
     ISOLATE=${f%_spades/};
     echo -n " /rds/general/user/sef17/home/msc_diss/data/gen/assemblies/${d}scaffolds.fasta" >> /rds/general/user/sef17/home/msc_diss/jobs/ASSEMBLY_stats.sh
 done
+echo -n " >> /rds/general/user/sef17/home/msc_diss/result/assembly_stats.txt"
