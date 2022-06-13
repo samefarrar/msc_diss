6db96df607978ebe257c958b0b76d3bb496f4078
Sam Farrar <samefarrar@gmail.com>
2022-06-13 10:00:35 +0100

    Fix underscore to dash for assembly stats

--- jobs/gen_assembly_stats_run.sh
+++ jobs/gen_assembly_stats_run.sh
@@ -1,7 +1,7 @@
-echo -n "~/msc_diss/ref/assembly_stats/assembly_stats -t" >> /rds/general/user/sef17/home/msc_diss/jobs/ASSEMBLY_stats.sh
+echo -n "~/msc_diss/ref/assembly-stats/assembly-stats -t" >> /rds/general/user/sef17/home/msc_diss/jobs/ASSEMBLY_stats.sh
 cd /rds/general/user/sef17/home/msc_diss/data/gen/assemblies
 for d in *_spades/; do
     ISOLATE=${f%_spades/};
     echo -n " /rds/general/user/sef17/home/msc_diss/data/gen/assemblies/${d}scaffolds.fasta" >> /rds/general/user/sef17/home/msc_diss/jobs/ASSEMBLY_stats.sh
 done
-echo -n " >> /rds/general/user/sef17/home/msc_diss/result/assembly_stats.txt"
+echo -n " >> /rds/general/user/sef17/home/msc_diss/result/assembly_stats.txt" >> /rds/general/user/sef17/home/msc_diss/jobs/ASSEMBLY_stats.sh
