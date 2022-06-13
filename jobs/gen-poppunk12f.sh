145bc4a6a7aebf4932b7d40561a95cb9f7cf9559
Sam Farrar <samefarrar@gmail.com>
2022-06-13 10:00:54 +0100

    Change to contigs.fasta to remove "guesses"

--- jobs/gen-poppunk12f.sh
+++ jobs/gen-poppunk12f.sh
@@ -1,3 +1,3 @@
 for d in *_spades/; do
-    echo -e "${d%_spades/}\t/rds/general/user/sef17/home/msc_diss/data/gen/assemblies/${d}scaffolds.fasta" >> all_12f.txt
+    echo -e "${d%_spades/}\t/rds/general/user/sef17/home/msc_diss/data/gen/assemblies/${d}contigs.fasta" >> all_12f.txt
 done
