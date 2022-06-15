dc0915995152ac1a4ddc541b950f13d8edfcba4c
Sam Farrar <samefarrar@gmail.com>
2022-06-15 12:28:23 +0100

    Update no_update visualise, inc memory and file locations

--- jobs/POPPUNK_no_update_visualise.sh
+++ jobs/POPPUNK_no_update_visualise.sh
@@ -3,7 +3,7 @@
 #PBS -o poppunk_visualize_nu.o
 #PBS -e poppunk_visualize_nu.e
 #PBS -l walltime=72:00:00
-#PBS -l select=1:ncpus=16:mem=500gb
+#PBS -l select=1:ncpus=16:mem=1000gb
 
 module load anaconda3/personal
 
@@ -11,4 +11,4 @@ source activate poppunk
 
 cd /rds/general/user/sef17/home/msc_diss/data/gen/assemblies
 
-poppunk_visualise --ref-db /rds/general/user/sef17/home/msc_diss/ref/reference_db/GPS_v5 --query-db /rds/general/user/sef17/home/msc_diss/result/no_update  --threads 8 --microreact --previous-clustering /rds/general/user/sef17/home/msc_diss/result/no_update/no_update_clusters.csv --info-csv /rds/general/user/sef17/home/msc_diss/ref/12F_epidata_long_lat.csv --output /rds/general/user/sef17/home/msc_diss/result/nu_visualise
+poppunk_visualise --ref-db /rds/general/user/sef17/home/msc_diss/ref/reference_db/GPS_v5 --query-db /rds/general/user/sef17/home/msc_diss/result/nu_assign --output /rds/general/user/sef17/home/msc_diss/result/nu_vis  --threads 16 --microreact --previous-clustering /rds/general/user/sef17/home/msc_diss/result/nu_assign/nu_assign_clusters.csv
