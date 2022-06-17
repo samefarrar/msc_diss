489a4ba8c920f95be8937bb9b1362a1cc3c5cb61
Sam Farrar <samefarrar@gmail.com>
2022-06-17 20:11:07 +0100

    Unsync draft bash script

--- jobs/transfer_NC.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-#PBS -N transfer_to_ephem
-#PBS -l walltime=72:00:00
-#PBS -l select=1:ncpus=4:mem=16gb
-
-mv /rds/general/user/sef17/home/msc_diss/data/orig/* /rds/general/user/sef17/ephemeral
