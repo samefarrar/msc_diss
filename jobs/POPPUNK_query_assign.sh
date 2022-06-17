9bfcdfab76919a318a5690eaf9f09b635425e77a
Sam Farrar <samefarrar@gmail.com>
2022-06-17 16:01:02 +0100

    Date parameter added to poppunk_assign output

--- jobs/POPPUNK_query_assign.sh
+++ jobs/POPPUNK_query_assign.sh
@@ -5,10 +5,12 @@
 #PBS -l walltime=72:00:00
 #PBS -l select=1:ncpus=16:mem=1000gb
 
+now=$(date +"%d_%m_%Y")
+
 module load anaconda3/personal
 
 source activate poppunk
 
 cd /rds/general/user/sef17/home/msc_diss/data/gen/assemblies
 
-poppunk_assign --db /rds/general/user/sef17/home/msc_diss/ref/reference_db/GPS_v6 --query /rds/general/user/sef17/home/msc_diss/data/gen/assemblies/all_12f.txt --output /rds/general/user/sef17/home/msc_diss/result/all_assign --threads 16 --external-clustering /rds/general/user/sef17/home/msc_diss/ref/reference_db/GPS_v6_external_clusters.csv --update-db --qc-filter prune --length-range 1900000 2500000 --prop-n 0.1
+poppunk_assign --db /rds/general/user/sef17/home/msc_diss/ref/reference_db/GPS_v6 --query /rds/general/user/sef17/home/msc_diss/data/gen/assemblies/all_12f.txt --output /rds/general/user/sef17/home/msc_diss/result/all_assign_$date --threads 16 --external-clustering /rds/general/user/sef17/home/msc_diss/ref/reference_db/GPS_v6_external_clusters.csv --update-db --qc-filter prune --length-range 1900000 2500000 --prop-n 0.1
