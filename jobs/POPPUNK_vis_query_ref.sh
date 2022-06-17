abefa17c2e622199747c37c2d110597ca88b71db
Sam Farrar <samefarrar@gmail.com>
2022-06-17 16:01:31 +0100

    Date parameter added POPPUNK_visualise

--- jobs/POPPUNK_vis_query_ref.sh
+++ jobs/POPPUNK_vis_query_ref.sh
@@ -7,6 +7,8 @@
 
 module load anaconda3/personal
 
+now=$(date +"%d_%m_%Y")
+
 source activate poppunk
 
-poppunk_visualise --ref-db /rds/general/user/sef17/home/msc_diss/ref/reference_db/GPS_v6 --query-db /rds/general/user/sef17/home/msc_diss/result/all_assign --previous-clustering /rds/general/user/sef17/home/msc_diss/result/all_assign/all_assign_clusters.csv --output /rds/general/user/sef17/home/msc_diss/result/vis_query_ref --threads 16 --microreact --external-clustering /rds/general/user/sef17/home/msc_diss/ref/reference_db/GPS_v6_external_clusters.csv
+poppunk_visualise --ref-db /rds/general/user/sef17/home/msc_diss/ref/reference_db/GPS_v6 --query-db /rds/general/user/sef17/home/msc_diss/result/all_assign --previous-clustering /rds/general/user/sef17/home/msc_diss/result/all_assign/all_assign_clusters.csv --output /rds/general/user/sef17/home/msc_diss/result/vis_query_ref_$now --threads 16 --microreact --external-clustering /rds/general/user/sef17/home/msc_diss/ref/reference_db/GPS_v6_external_clusters.csv
