8f613ba935e47a34fe40fa07411e587996ff6ff6
Sam Farrar <samefarrar@gmail.com>
2022-06-17 15:59:00 +0100

    Change from v5 to v6 of the GPS reference database

--- jobs/POPPUNK_no_update_visualise.sh
+++ jobs/POPPUNK_no_update_visualise.sh
@@ -9,6 +9,8 @@ module load anaconda3/personal
 
 source activate poppunk
 
+now=$(date +"%d_%m_%Y")
+
 cd /rds/general/user/sef17/home/msc_diss/data/gen/assemblies
 
-poppunk_visualise --ref-db /rds/general/user/sef17/home/msc_diss/ref/reference_db/GPS_v5 --query-db /rds/general/user/sef17/home/msc_diss/result/nu_assign --output /rds/general/user/sef17/home/msc_diss/result/nu_vis  --threads 16 --microreact --previous-clustering /rds/general/user/sef17/home/msc_diss/result/nu_assign/nu_assign_clusters.csv --external-clustering /rds/general/user/sef17/home/msc_diss/ref/reference_db/GPS_v6_external_clusters.csv
+poppunk_visualise --ref-db /rds/general/user/sef17/home/msc_diss/ref/reference_db/GPS_v6 --query-db /rds/general/user/sef17/home/msc_diss/result/nu_assign --output /rds/general/user/sef17/home/msc_diss/result/nu_vis_$now  --threads 16 --microreact --previous-clustering /rds/general/user/sef17/home/msc_diss/result/nu_assign/nu_assign_clusters.csv --external-clustering /rds/general/user/sef17/home/msc_diss/ref/reference_db/GPS_v6_external_clusters.csv
